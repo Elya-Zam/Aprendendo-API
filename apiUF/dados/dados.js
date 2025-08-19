f6237210be5c3230deb13665dec6d76c77fbd02e
Elya-Zam <elyazssousa@gmail.com>
2025-08-19 09:08:23 -0400

    1 aula de beck2

---
+++ apiUF/dados/dados.js
@@ -0,0 +1,165 @@
+const colecaoUF = [
+    {
+        id: 1,
+        uf: 'AC',
+        nome: 'Acre',
+    },
+
+    {
+        id: 2,
+        uf: 'AL',
+        nome: 'Alagoas',
+    },
+
+    {
+        id: 3,
+        uf: 'AM',
+        nome: 'Amazonas',
+    },
+
+    {
+        id: 4,
+        uf: 'AP',
+        nome: 'Amapa',
+    },
+
+    {
+        id: 5,
+        uf: 'BA',
+        nome: 'Bahina',
+    },
+
+    {
+        id: 6,
+        uf: 'CE',
+        nome: 'Ceara',
+    },
+
+    {
+        id: 7,
+        uf: 'DF',
+        nome: 'Distrito Federal',
+    },
+
+    {
+        id: 8,
+        uf: 'ES',
+        nome: 'Espirito santo',
+    },
+
+    {
+        id: 9,
+        uf: 'GO',
+        nome: 'Goias',
+    },
+
+    {
+        id: 10,
+        uf: 'MA',
+        nome: 'Maranhão',
+    },
+
+    {
+        id: 11,
+        uf: 'MG',
+        nome: 'Minas gerais',
+    },
+
+    {
+        id: 12,
+        uf: 'MS',
+        nome: 'Mato grosso do sul',
+    },
+
+    {
+        id: 13,
+        uf: 'MT',
+        nome: 'Mato grosso',
+    },
+
+    {
+        id: 14,
+        uf: 'PA',
+        nome: 'Pára',
+    },
+
+    {
+        id: 15,
+        uf: 'PB',
+        nome: 'Paraiba',
+    },
+
+    {
+        id: 16,
+        uf: 'Pe',
+        nome: 'Pernanbuco',
+    }, 
+   
+    {
+        id: 17,
+        uf: 'PI',
+        nome: 'Piaui',
+    },
+
+    {
+        id: 18,
+        uf: 'PR',
+        nome: 'Parana',
+    },  
+
+    {
+        id: 19,
+        uf: 'RJ',
+        nome: 'Rio de janeiro',
+    },  
+
+    {
+        id: 20,
+        uf: 'RN',
+        nome: 'Rio grande do norte',
+    },
+
+    {
+        id: 21,
+        uf: 'RO',
+        nome: 'Rondonio',
+    },
+
+    {
+        id: 22,
+        uf: 'RR',
+        nome: 'Roraima',
+    },
+
+    {
+        id: 23,
+        uf: 'RS',
+        nome: 'Rio grande do sul',
+    }, 
+
+    {
+        id: 24,
+        uf: 'SC',
+        nome: 'Santa catarina',
+    },  
+
+    {
+        id: 25,
+        uf: 'SE',
+        nome: 'Sergipe',
+    },   
+
+    {
+        id: 26,
+        uf: 'SP',
+        nome: 'São paulo ',
+    },   
+
+    {
+        id: 27,
+        uf: 'TO',
+        nome: 'Tocantins',
+    }
+]
+
+export default colecaoUF;
