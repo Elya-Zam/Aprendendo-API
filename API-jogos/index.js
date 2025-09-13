8d2b99c761911c2fc24c481d14041cfc132bac64
Elyã <zam.sousa@estudante.ifro.edu.br>
2025-09-13 15:56:58 -0400

    api de jogos

--- API-jogos/index.js
+++ API-jogos/index.js
@@ -1,8 +1,60 @@
-const express = require('express');
+import express from 'express';
+import lista from './jogos.js';  
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API de Jogos funcionando!');
+const buscaPorGenero = (genero) => { 
+  return lista.filter(jogo => jogo.genero.toLowerCase() === genero.toLowerCase());
+};
+
+const buscaPorPlataforma = (plataforma) => { 
+  return lista.filter(jogo => jogo.plataforma.toLowerCase() === plataforma.toLowerCase());
+};
+
+app.get('/jgs', (req, res) => {
+  const genero = req.query.genero;
+  const plataforma = req.query.plataforma;
+
+  if (plataforma) {
+    const resultado = buscaPorPlataforma(plataforma);
+    if (resultado.length > 0) {
+      return res.json(resultado);
+    } else {
+      return res.status(404).json({ erro: 'Nenhum jogo encontrado para o gênero especificado' });
+    }
+  };
+
+  if (genero) {
+    const resultado = buscaPorGenero(genero);
+    if (resultado.length > 0) {
+      return res.json(resultado);
+    } else {
+      return res.status(404).json({ erro: 'Nenhum jogo encontrado para o gênero especificado' });
+    }
+  }
+
+  res.json(lista);
+});
+
+app.get('/jgs/:id', (req, res) => {
+  const jgID = parseInt(req.params.id);
+  let mensagemErro = '';
+  let jg;
+  
+  if (!(isNaN(jgID))) {
+    jg = lista.find(j => j.id === jgID);
+    if (!jg) {
+      mensagemErro = 'Jogo não encontrado';
+    }
+  } else {
+    res.status(400).json({ "erro": "ID inválido" });
+  }
+
+  if (jg) {
+    res.json(jg);
+  } else {  
+    res.status(404).json({ "erro": mensagemErro });
+  }
 });
 
 app.listen(3000, () => {
---
+++ API-jogos/jogos.js
@@ -0,0 +1,39 @@
+const jogos = [
+  {
+    id: 1,
+    nome: 'The Legend of Zelda: Breath of the Wild',
+    genero: 'Aventura',
+    plataforma: 'Nintendo Switch',
+    ano: 2017
+  },
+  {
+    id: 2,
+    nome: 'God of War',
+    genero: 'Ação',
+    plataforma: 'PlayStation 4',
+    ano: 2018
+  },
+  {
+    id: 3,
+    nome: 'Minecraft',
+    genero: 'Sandbox',
+    plataforma: 'Multiplataforma',
+    ano: 2011
+  },
+  {
+    id: 4,
+    nome: 'FIFA 22',
+    genero: 'Esporte',
+    plataforma: 'Multiplataforma',
+    ano: 2021
+  },
+  {
+    id: 5,
+    nome: 'Grand Theft Auto V',
+    genero: 'Ação/Aventura',
+    plataforma: 'Multiplataforma',
+    ano: 2013
+  }
+];
+
+export default jogos;
